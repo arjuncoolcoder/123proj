e427c68bb6ee5819d5be3eb7322c2b0e267e5386
arjuncoolcoder <113683962+arjuncoolcoder@users.noreply.github.com>
2023-08-11 22:16:23 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+
+function setup()
+{
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded); 
+  poseNet.on('pose', gotPoses);
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+      console.log(results);
+  }
+}
+
+function draw()
+{
+      background('#0000FF');
+}
+
+  function modelLoaded()
+  {
+      console.log('PoseNet Is Initialised!');
+  }
